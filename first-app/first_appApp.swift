834125d63438798252c104869d07b82469db9e8e
İsmail Ertuğlu <ismailertgl@gmail.com>
2022-11-03 22:42:40 +0300

    Initial Commit

---
+++ first-app/first_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_appApp.swift
+//  first-app
+//
+//  Created by İsmail Ertuğlu on 3.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct first_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
